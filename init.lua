42b932e5b89cc82d0524a2946dbdaf54c647d5cd
Dylan Russell <dyl.russell@gmail.com>
2025-04-21 14:05:44 +0900

    working on init.

--- init.lua
+++ init.lua
@@ -536,7 +536,7 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+        ensure_installed = {}, -- explicitly set to an empty table (install defined above using via mason-tool-installer)
         automatic_installation = false,
         handlers = {
           function(server_name)
